a0b9953f883dabcb5d5a253dc84a6c6f43bd6ea5
Marcel Czuryszkiewicz <cczuryszkiewicz@gmail.com>
2023-03-03 18:01:41 +0100

    using backgroundremover cli

--- remove.py
+++ remove.py
@@ -1,7 +1,12 @@
-from rembg import remove 
-from PIL import Image, ImageDraw, ImageFont
+
 import glob, os
 from pathlib import Path
+
+import subprocess
+import locale
+from PIL import Image, ImageDraw, ImageFont
+import subprocess
+import time
 
 
 main_output_folder = "output"
@@ -17,30 +22,38 @@ for path in Path("input").rglob("*.png"):
 for x in path_list:
     for key, value in x.items():
 
-        img = Image.open(os.path.join(value, key))
-        output = remove(img)
-
-        #saving without watermark
-        output_path = os.path.join(main_output_folder, "nonwatermark", *value.parts[1:])
+        output_path = os.path.join(main_output_folder, *value.parts[1:])
         if not os.path.exists(output_path):
             os.makedirs(output_path)
+
+        output_path_watermark = os.path.join(main_output_folder, "watermark", *value.parts[1:])
+        if not os.path.exists(output_path_watermark):
+            os.makedirs(output_path_watermark)
+
+        input_file_location = os.path.join(value, key)
+        output_file_location = os.path.join(main_output_folder, *value.parts[1:], key)
+
+        cli_comand = f"backgroundremover -i {input_file_location} -m u2netp -o {output_file_location}"
         
-        output.save(os.path.join(main_output_folder, "nonwatermark", *value.parts[1:], key))
-        ##########
-        
+        out = str(subprocess.check_output(cli_comand).decode(encoding="utf-8", errors="ignore"))
+
+        try:
+
+            out = str(subprocess.check_output(cli_comand).decode(encoding="utf-8", errors="ignore"))
+
+        except subprocess.CalledProcessError as ex:
+            print(f"Error with {input_file_location}")
+
+        img = Image.open(output_file_location)
         t = ImageDraw.Draw(img, "RGBA")
         #font type and scale
         fnt = ImageFont.truetype("comicbd.ttf", 40)
         
         #position of watermark X Y
         t.text((img.size[0]/1.5, img.size[1]/2), watermark_txt, font=fnt, fill=(255, 255, 255, 120))
+        img.save(os.path.join(output_path_watermark, key))
+        
 
-        output_watermark = remove(img)
-        output_path = os.path.join(main_output_folder, *value.parts[1:])
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
-
-        output_watermark.save(os.path.join(main_output_folder, *value.parts[1:], key))
 
         
 
